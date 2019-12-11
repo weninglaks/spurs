bfa16f103f50122b4f26c3fa85c98f7fbf16cca2
Cyan9145 <weninglaksito@gmail.com>
2019-12-11 18:26:02 +0700

    uncomment service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-// workbox.precaching.precacheAndRoute([
-//     { url: '/index.html', revision: '1' },
-//     { url: '/nav.html', revision: '1' },
-// 		{ url: '/manifest.json', revision: '1' },
-// 		{ url: '/favicon.png', revision: '1' },
-//     { url: '/css/materialize.min.css', revision: '1' },
-// 		{ url: '/css/style.css', revision: '1' },
-//     { url: '/js/materialize.min.js', revision: '1' },
-// 		{ url: '/js/idb.js', revision: '1' },
-// 		{ url: '/js/db.js', revision: '1' },
-//     { url: '/js/script.js', revision: '1' },
-// 		{ url: '/js/api.js', revision: '1' }
-// ]);
+workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+		{ url: '/manifest.json', revision: '1' },
+		{ url: '/favicon.png', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+		{ url: '/css/style.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+		{ url: '/js/idb.js', revision: '1' },
+		{ url: '/js/db.js', revision: '1' },
+    { url: '/js/script.js', revision: '1' },
+		{ url: '/js/api.js', revision: '1' }
+]);
 
 
 workbox.routing.registerRoute(
